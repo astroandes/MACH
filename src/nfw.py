66c13e96578e498c465c39ac7d48d7ff50f39026
Christian Poveda <christianpoved4@gmail.com>
2014-03-03 08:22:22 -0500

    corrected typos

--- src/nfw.py
+++ src/nfw.py
@@ -8,36 +8,36 @@ import numpy as np
 # Returns:
 #     a numpy array with the mass/density profile
 
-def mass(r,rho0,rs):
-    return 4*np.pi*(rs**3)*rho0*(np.log(1+(r/rs))-(1+(rs/r))**(-1))
+def mass(r,a,b):
+    return 4*np.pi*a*(b**3)*(np.log(1+r/b)-(1+(r/b))**(-1))
 
-def density(r,rho0,rs):
-    return rho0*((r/rs)*(1+(r/rs))**2)**(-1)
+def density(r,a,b):
+    return a*((r/b)*(1+(r/b))**2)**(-1)
 
 # Profiles in logarithmic scale
 # Parameters:
 #     logr: numpy array with the logarithm of the radial distances from the center of the halo
-#     rho0: mean density
-#     rs: scale radius
+#     a: mean density
+#     b: scale radius
 # Returns:
 #     a numpy array with the logarithm of the mass/density profile
 
-def logmass(logr,rho0,rs):
-    return np.log(4*np.pi)+3*np.log(rho0)+3*np.log(rs)+np.log(np.log(1+np.exp(logr)/rs)-(1+rs*np.exp(-logr))**(-1))
+def logmass(logr,a,b):
+    return np.log(4*np.pi)+ np.log(a)+3*np.log(b)+np.log(np.log(1+np.exp(logr)/b)-(1+np.exp(logr)/b)**(-1))
 
-def logdensity(logr,rho0,rs):
-    return np.log(rho0)-np.log(rs)+logr+2*np.log(1+np.exp(logr)/rs)
+def logdensity(logr,a,b):
+    return np.log(a)+np.log(b)-logr-2*np.log(1+np.exp(logr)/b)
 
 # Profiles in logarithmic scale with logarithmic parameters
 # Parameters:
 #     logr: numpy array with the logarithm of the radial distances from the center of the halo
-#     logrho0: logarithm of the mean density
-#     logrs: logarithm of the scale radius
+#     loga: logarithm of the mean density
+#     logb: logarithm of the scale radius
 # Returns:
 #     a numpy array with the logarithm of the mass/density profile
 
-def loglogmass(logr,logrho0,logrs):
-    return np.log(4*np.pi)+3*logrs+logrho0+np.log(np.log(1+np.exp(logr-logrs))-(1+np.exp(logrs-logr))**(-1))
+def loglogmass(logr,loga,logb):
+    return np.log(4*np.pi)+ loga+3*logb+np.log(np.log(1+np.exp(logr-logb))-(1+np.exp(logr-logb))**(-1))
 
-def loglogdensity(logr,logrho0,logrs):
-    return logrho0-logrs+logr+2*np.log(1+np.exp(logr-logrs))
+def loglogdensity(logr,loga,logb):
+    return loga+logb-logr-2*np.log(1+np.exp(logr-logb))
