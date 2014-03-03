ae24d6a480b4bd1885ce2396a4ee56b5cd7e6ea3
Christian Poveda <christianpoved4@gmail.com>
2014-03-03 00:27:51 -0500

    updated test tool

--- src/test.py
+++ src/test.py
@@ -5,8 +5,16 @@ mark = np.loadtxt(sys.argv[2])
 
 mcmc = mark[mark[:,0].argsort()]
 
-pylab.plot(orig[:,1],mcmc[:,4])
-pylab.show()
+orig_rs = orig[:,2]
+mcmc_rs = mcmc[:,7]
+mcmc_rs_min = mcmc[:,8]
+mcmc_rs_max = mcmc[:,9]
+rs_sigma = np.array([mcmc_rs_max-mcmc_rs,mcmc_rs-mcmc_rs_min])
 
-pylab.plot(orig[:,2],mcmc[:,7])
-pylab.show()
+pylab.errorbar(orig_rs,mcmc_rs,yerr=rs_sigma,fmt='.r',elinewidth=1,capsize=2,capthick=1)
+pylab.plot(orig_rs,orig_rs,'--k')
+pylab.title('Scale Radius (Test)')
+pylab.xlabel('Original')
+pylab.ylabel('Montecarlo-NFW')
+pylab.savefig('test.png',dpi=150)
+pylab.close()
