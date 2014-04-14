26dddf412e3d36d31cc198547f708c49f57c3825
Christian Poveda <christianpoved4@gmail.com>
2014-04-13 22:00:28 -0500

    new test

--- data/test/test.py
+++ data/test/test.py
@@ -1,95 +1,125 @@
 import numpy as np, pylab
 
-real = np.loadtxt('parameters.dat',delimiter=',')
-auto = np.loadtxt('auto_center.csv',delimiter=',')
-gets = np.loadtxt('gets_center.csv',delimiter=',')
+data = np.loadtxt('gets_center.csv',delimiter=',')
 
-auto = auto[auto[:,0].argsort()]
-gets =gets[gets[:,0].argsort()]
+orig = np.loadtxt('parameters.dat',delimiter=',')
+data = data[data[:,0].argsort()]
 
-real_a = real[:,0]
-auto_a,auto_amin,auto_amax = auto[:,4],auto[:,5],auto[:,6]
-gets_a,gets_amin,gets_amax = gets[:,4],gets[:,5],gets[:,6]
-auto_asigma = np.array([auto_amax-auto_a,auto_a-auto_amin])
-gets_asigma = np.array([gets_amax-gets_a,gets_a-gets_amin])
+c_bdmv = data[:,4]
+c_bdmw = data[:,7]
 
-real_b = real[:,1]
-auto_b,auto_bmin,auto_bmax = auto[:,7],auto[:,8],auto[:,9]
-gets_b,gets_bmin,gets_bmax = gets[:,7],gets[:,8],gets[:,9]
-auto_bsigma = np.array([auto_bmax-auto_b,auto_b-auto_bmin])
-gets_bsigma = np.array([gets_bmax-gets_b,gets_b-gets_bmin])
+r_bdmv = data[:,10]
+r_bdmw = data[:,11]
 
+rs_bdmv = r_bdmv/c_bdmv
+rs_bdmw = r_bdmw/c_bdmw
 
-pylab.plot(real_a,real_a,'-k')
-pylab.errorbar(real_a,auto_a,yerr=auto_asigma,fmt='.r',elinewidth=1,capsize=2,capthick=1)
-pylab.xscale('log')
-pylab.yscale('log')
-pylab.xlabel('$Real$')
-pylab.ylabel('$Obtained$')
-pylab.title('$\\rho_0$ with center at 0')
-pylab.savefig('rho0_auto.png',dpi=300)
+rs = orig[:,1]
+
+pylab.title('$r_s$')
+pylab.plot(rs,rs,'-k')
+pylab.plot(rs,rs_bdmv,'ob')
+pylab.xlabel('Real')
+pylab.ylabel('BDMV')
+pylab.savefig('gets_bdmv.png',dpi=300)
 pylab.close()
 
-pylab.plot(real_b,real_b,'-k')
-pylab.errorbar(real_b,auto_b,yerr=auto_bsigma,fmt='.r',elinewidth=1,capsize=2,capthick=1)
-pylab.xscale('log')
-pylab.yscale('log')
-pylab.xlabel('$Real$')
-pylab.ylabel('$Obtained$')
-pylab.title('$R_s$ with center at 0')
-pylab.savefig('Rs_auto.png',dpi=300)
+pylab.title('$r_s$')
+pylab.plot(rs,rs-rs,'-k')
+pylab.plot(rs,rs_bdmv-rs,'ob')
+pylab.xlabel('Real')
+pylab.ylabel('BDMV-Real')
+pylab.savefig('gets_bdmv-real.png',dpi=300)
 pylab.close()
 
-pylab.plot(real_a,real_a,'-k')
-pylab.errorbar(real_a,gets_a,yerr=gets_asigma,fmt='.r',elinewidth=1,capsize=2,capthick=1)
-pylab.xscale('log')
-pylab.yscale('log')
-pylab.xlabel('$Real$')
-pylab.ylabel('$Obtained$')
-pylab.title('$\\rho_0$')
-pylab.savefig('rho0_gets.png',dpi=300)
+pylab.title('$r_s$')
+pylab.plot(rs,np.abs(rs_bdmv-rs)/rs,'ob')
+pylab.xlabel('Real')
+pylab.ylabel('|BDMV-Real|/Real')
+pylab.savefig('gets_bdmv_relative.png',dpi=300)
 pylab.close()
 
-pylab.plot(real_b,real_b,'-k')
-pylab.errorbar(real_b,gets_b,yerr=gets_bsigma,fmt='.r',elinewidth=1,capsize=2,capthick=1)
-pylab.xscale('log')
-pylab.yscale('log')
-pylab.xlabel('$Real$')
-pylab.ylabel('$Obtained$')
-pylab.title('$R_s$')
-pylab.savefig('Rs_gets.png',dpi=300)
+pylab.title('$r_s$')
+pylab.plot(rs,np.abs(rs_bdmw-rs)/rs,'or')
+pylab.xlabel('Real')
+pylab.ylabel('|BDMW-Real|/Real')
+pylab.savefig('gets_bdmw_relative.png',dpi=300)
 pylab.close()
 
-pylab.plot(real_a,np.abs(auto_a-real_a)/real_a,'or')
-pylab.xscale('log')
-pylab.xlabel('$Real$')
-pylab.ylabel('$|Obtained-Real|/Real$')
-pylab.title('$\\rho_0$ with center at 0')
-pylab.savefig('rho0_auto_relative.png',dpi=300)
+pylab.title('$r_s$')
+pylab.plot(rs,rs,'-k')
+pylab.plot(rs,rs_bdmw,'or')
+pylab.xlabel('Real')
+pylab.ylabel('BDMW')
+pylab.savefig('gets_bdmw.png',dpi=300)
 pylab.close()
 
-pylab.plot(real_b,np.abs(auto_b-real_b)/real_b,'or')
-pylab.xscale('log')
-pylab.xlabel('$Real$')
-pylab.ylabel('$|Obtained-Real|/Real$')
-pylab.title('$R_s$ with center at 0')
-pylab.savefig('Rs_auto_relative.png',dpi=300)
+pylab.title('$r_s$')
+pylab.plot(rs,rs-rs,'-k')
+pylab.plot(rs,rs_bdmw-rs,'or')
+pylab.xlabel('Real')
+pylab.ylabel('BDMW-Real')
+pylab.savefig('gets_bdmw-real.png',dpi=300)
 pylab.close()
 
-pylab.plot(real_a,np.abs(gets_a-real_a)/real_a,'or')
-pylab.xscale('log')
-pylab.xlabel('$Real$')
-pylab.ylabel('$|Obtained-Real|/Real$')
-pylab.title('$\\rho_0$')
-pylab.savefig('rho0_gets_relative.png',dpi=300)
+data = np.loadtxt('auto_center.csv',delimiter=',')
+
+orig = np.loadtxt('parameters.dat',delimiter=',')
+data = data[data[:,0].argsort()]
+
+c_bdmv = data[:,4]
+c_bdmw = data[:,7]
+
+r_bdmv = data[:,10]
+r_bdmw = data[:,11]
+
+rs_bdmv = r_bdmv/c_bdmv
+rs_bdmw = r_bdmw/c_bdmw
+
+rs = orig[:,1]
+
+pylab.title('$r_s$')
+pylab.plot(rs,rs,'-k')
+pylab.plot(rs,rs_bdmv,'ob')
+pylab.xlabel('Real')
+pylab.ylabel('BDMV')
+pylab.savefig('auto_bdmv.png',dpi=300)
 pylab.close()
 
-pylab.plot(real_b,np.abs(gets_b-real_b)/real_b,'or')
-pylab.xscale('log')
-pylab.xlabel('$Real$')
-pylab.ylabel('$|Obtained-Real|/Real$')
-pylab.title('$R_s$')
-pylab.savefig('Rs_gets_relative.png',dpi=300)
+pylab.title('$r_s$')
+pylab.plot(rs,rs-rs,'-k')
+pylab.plot(rs,rs_bdmv-rs,'ob')
+pylab.xlabel('Real')
+pylab.ylabel('BDMV-Real')
+pylab.savefig('auto_bdmv-real.png',dpi=300)
 pylab.close()
 
+pylab.title('$r_s$')
+pylab.plot(rs,rs,'-k')
+pylab.plot(rs,rs_bdmw,'or')
+pylab.xlabel('Real')
+pylab.ylabel('BDMW')
+pylab.savefig('auto_bdmw.png',dpi=300)
+pylab.close()
 
+pylab.title('$r_s$')
+pylab.plot(rs,rs-rs,'-k')
+pylab.plot(rs,rs_bdmw-rs,'or')
+pylab.xlabel('Real')
+pylab.ylabel('BDMW-Real')
+pylab.savefig('auto_bdmw-real.png',dpi=300)
+pylab.close()
+
+pylab.title('$r_s$')
+pylab.plot(rs,np.abs(rs_bdmv-rs)/rs,'ob')
+pylab.xlabel('Real')
+pylab.ylabel('|BDMV-Real|/Real')
+pylab.savefig('auto_bdmv_relative.png',dpi=300)
+pylab.close()
+
+pylab.title('$r_s$')
+pylab.plot(rs,np.abs(rs_bdmw-rs)/rs,'or')
+pylab.xlabel('Real')
+pylab.ylabel('|BDMW-Real|/Real')
+pylab.savefig('auto_bdmw_relative.png',dpi=300)
+pylab.close()
