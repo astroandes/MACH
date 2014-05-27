ae80b6b8a58c15a1f29f1744d1a4af465e90c3ca
Christian Poveda <christianpoved4@gmail.com>
2014-05-26 23:27:08 -0500

    sorted results

--- src/glue_test.py
+++ src/glue_test.py
@@ -6,7 +6,7 @@ filenames = sorted([directory+'/'+element for element in total_list if 'result' 
 
 
 export = open(directory+'/results.csv', 'w')
-head = 'Id,x_center,y_center,z_center,c_bdmv,c_bdmv_max,c_bdmv_min,c_bdmw,c_bdmw_max,c_bdmw_min,r_vir_bdmv,r_vir_bdmw,n_vir_bdmv,n_vir_bdmw,n_tot'
+head='Id,x_center,y_center,z_center,conc,c_max,c_min'
 
 with export as outfile:
     for fname in filenames:
@@ -17,4 +17,4 @@ with export as outfile:
 data = np.loadtxt(directory+'/results.csv',delimiter=',')
 data = data[data[:,0].argsort()]
 
-np.savetxt(directory+'/results.csv',data,fmt='%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d',header=head)
+np.savetxt(directory+'/results.csv',data,fmt='%d,%f,%f,%f,%f,%f,%f',header=head)
