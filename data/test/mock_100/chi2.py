aa7232ae34c85c9e5a3c39f15370117021a709e8
Christian Poveda <christianpoved4@gmail.com>
2014-08-30 15:19:06 -0500

    added chi2 plot

--- data/test/mock_100/chi2.py
+++ data/test/mock_100/chi2.py
@@ -22,7 +22,7 @@ pylab.savefig('vlct.png',dpi=300)
 pylab.close()
 
 
-print chi2(orig,vlct[:,1]),chi2(orig,vlct[:,2]),chi2(orig,vlct[:,3]),chi2(orig,vlct[:,4])
+chi_vlct = [chi2(orig,vlct[:,1]),chi2(orig,vlct[:,2]),chi2(orig,vlct[:,3]),chi2(orig,vlct[:,4])]
 
 orig = dens[:,0]
 
@@ -39,7 +39,7 @@ pylab.legend(loc=4, borderaxespad=0.5)
 pylab.savefig('dens.png',dpi=300)
 pylab.close()
 
-print chi2(orig,dens[:,1]),chi2(orig,dens[:,2]),chi2(orig,dens[:,3])
+chi_dens =[chi2(orig,dens[:,1]),chi2(orig,dens[:,2]),chi2(orig,dens[:,3])]
 
 orig = mass[:,0]
 
@@ -55,4 +55,18 @@ pylab.legend(loc=4, borderaxespad=0.5)
 pylab.savefig('mass.png',dpi=300)
 pylab.close()
 
-print chi2(orig,mass[:,1]),chi2(orig,mass[:,2]),chi2(orig,mass[:,3]),chi2(orig,mass[:,4])
+chi_mass = [chi2(orig,mass[:,1]),chi2(orig,mass[:,2]),chi2(orig,mass[:,3]),chi2(orig,mass[:,4])]
+
+pylab.plot([20,200,2000,20000],chi_mass,'or',label='$Mass$')
+pylab.plot([20,200,2000,20000],chi_mass,'-r')
+pylab.plot([20,200,2000,20000],chi_vlct,'sg',label='$Velocty$')
+pylab.plot([20,200,2000,20000],chi_vlct,'-g')
+pylab.plot([200,2000,20000],chi_dens,'^b',label='$Density$')
+pylab.plot([200,2000,20000],chi_dens,'-b')
+pylab.xscale('log')
+pylab.yscale('log')
+pylab.legend()
+pylab.xlabel('$Number\ of\ Points$',fontsize=20)
+pylab.ylabel('$\chi^{2}$',fontsize=20)
+pylab.savefig('chi2.png',dpi=200)
+pylab.close()
