15b9b798ba087bd38546b40507dbe275a78c69e5
Christian POveda <christianpoved4@gmail.com>
2014-09-09 14:22:06 -0500

    now with explanation

--- src/main.py
+++ src/main.py
@@ -53,40 +53,47 @@ def run(directories,process_number):
         count = count + 1
         print '\rWorking with file '+str(count)+' of '+str(len(directories))+' in process '+str(process_number)
 
+    # Creates the "data" array with the information from the file in "path"
         path = os.path.expanduser('./'+str(sys.argv[1])+'/'+filename)
         data = np.loadtxt(open(path, 'r'), delimiter=",",skiprows=int(sys.argv[5]))
 
+    # Gets the cartesian coordinates for each particle in the halo and the number of particles
         x = data[:,int(sys.argv[2])]
         y = data[:,int(sys.argv[3])]
         z = data[:,int(sys.argv[4])]
-
         n_points = len(x)
-        
+
+    # Exports a file with the cartesian coordinates of each particle
         file_id = int(filename.split('_')[1])
         positions_name ='positions_'+str(file_id)+'.dat'
         potential_name ='potential_'+str(file_id)+'.dat'
         results_folder ='./results_'+now+'/'+str(file_id)
         open(positions_name, "w").write('\n'.join('%lf,%lf,%lf' % (x[i],y[i],z[i]) for i in range(n_points)))
 
+    # Runs the executable "potential.out" that will get the potential energy for each particle
         os.system('./potential.out '+positions_name+' '+potential_name)
 
+    # Finds the particle with the lowest potential and puts it as the new origin
         potential = np.loadtxt(open(potential_name, 'r'))
         maximum = np.argmax(potential)
-
         x_center,y_center,z_center = x[maximum],y[maximum],z[maximum]
-        
-        os.system('rm '+potential_name+' '+positions_name)
-    
+
+    # Gets the radial distance from the new origin to each particle and sorts it
         r_values = np.sort(np.sqrt((x-x_center)**2 + (y-y_center)**2 + (z-z_center)**2), kind='quicksort')
-        
+    
+    # Removes the files used by "potential.out"
+        os.system('rm '+potential_name+' '+positions_name)
+
+    # Gets the mass for each radius and removes the particle in the origin
         mass = mass_element*np.arange(1,n_points,1)
         radius = np.delete(r_values,0)
 
+    # Gets the average density in function of the radius
         avg_density = mass/((4.0/3.0)*np.pi*(radius**3))
         rho_back = mass_element*(2048.0/1000.0)**3
 
+    # Gets the virial radius
         crit = 740.0
-
         vir_index = np.argmin(np.abs(avg_density-crit*rho_back))
 
         if np.argmin(np.abs(avg_density-crit*rho_back)) > 1:
@@ -95,30 +102,30 @@ def run(directories,process_number):
             r_vir = radius[-1]
             vir_index = len(avg_density)-1
 
+    # Removes the particles that have a greater radius than the virial radius
         mass = np.resize(mass,vir_index+1)
         radius = np.resize(radius,vir_index+1)
 
+    # Normalizes the mass and radius
         mass = mass/mass[-1]
         radius = radius/radius[-1]
 
+    # Does the Metropolis algorithm in order to find the concentration
+        
         n_iterations = 50000
 
         step = np.array([np.log(1.03)])
         guess = np.array([np.log(10)])
         reest = [lambda x: x >= 0]
 
-        chi_2 = lambda p : mcmc.chi2(p,nfw.loglogmass_norm,np.log(radius),np.log(mass),np.ones(len(radius)))
-    
+        chi_2 = lambda p : mcmc.chi2(p,nfw.loglogmass_norm,np.log(radius),np.log(mass),np.ones(len(radius)))    
         walk,chi2 = mcmc.walker(chi_2,guess,step,n_iterations,reest)
-
         walk = walk[0]
-        
         log_c = walk[np.argmin(chi2)]
-
         c = np.exp(log_c)
-    
         c_max, c_min = np.exp(fit.error_bars(walk,log_c,'log'))
-    
+
+    # Generates plots
         if (plt == 1):
 
             os.system('mkdir '+results_folder)
@@ -145,7 +152,8 @@ def run(directories,process_number):
             pylab.close()
 
             os.chdir('../../')
-
+            
+    # Writes the results for the halo
         export = open(filename_export, 'a')
         line = [[file_id,x_center,y_center,z_center,c,c_max,c_min,r_vir,len(mass),n_points]]
         np.savetxt(export,line,fmt=['%d','%lf','%lf','%lf','%lf','%lf','%lf','%lf','%d','%d'],delimiter=',')
