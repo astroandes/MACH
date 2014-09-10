feb87cc2a72c50daebfef500c96dffed6ed320fb
Christian POveda <christianpoved4@gmail.com>
2014-09-09 21:13:46 -0500

    updated velocity method

--- src/velocity.py
+++ src/velocity.py
@@ -1,22 +1,23 @@
 import numpy as np, pylab, math, sys, timeit, os, random, datetime, nfw, mcmc,fit , plotter, multiprocessing, time
 from scipy.optimize import fsolve
 
+# Gets the concentration and virial radius for several haloes
 
-G = 4.49E-15 # Mpc^3/(solar masses Gyr^2)
-processes = int(sys.argv[6])
+config = open('config.div','r').readline().split()
+print config
+processes = int(config[5])
 now = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-mass_element = 	8.721E9    
+mass_element = 1.0   
 plt = 1
-total_list = os.listdir('./'+str(sys.argv[1]))
+total_list = os.listdir(str(config[0]))
 len_list = len(total_list)
 lists = [total_list[i*len_list // processes: (i+1)*len_list //processes] for i in range(processes)]
 jobs = []
 
-try:
-    if (sys.argv[7]=='noplot'):
-        print 'No Plotting mode enabled'
-        plt = 0
-except:
+if (config[7]=='False'):
+    print 'No Plotting mode enabled'
+    plt = 0
+else:
     plt = 1
  
 os.system('mkdir results_'+now)
@@ -37,58 +38,93 @@ def run(directories,process_number):
         count = count + 1
         print '\rWorking with file '+str(count)+' of '+str(len(directories))+' in process '+str(process_number)
 
-        path = os.path.expanduser('./'+str(sys.argv[1])+'/'+filename)
-        data = np.loadtxt(open(path, 'r'), delimiter=",",skiprows=int(sys.argv[5]))
+    # Creates the "data" array with the information from the file in "path"
+        path = os.path.expanduser('./'+str(config[0])+'/'+filename)
+        data = np.loadtxt(open(path, 'r'), delimiter=",",skiprows=int(config[4]))
 
-        x = data[:,int(sys.argv[2])]
-        y = data[:,int(sys.argv[3])]
-        z = data[:,int(sys.argv[4])]
-
+    # Gets the cartesian coordinates for each particle in the halo and the number of particles
+        x = data[:,int(config[1])]
+        y = data[:,int(config[2])]
+        z = data[:,int(config[3])]
         n_points = len(x)
-        
+
+    # Exports a file with the cartesian coordinates of each particle
         file_id = int(filename.split('_')[1])
         positions_name ='positions_'+str(file_id)+'.dat'
         potential_name ='potential_'+str(file_id)+'.dat'
         results_folder ='./results_'+now+'/'+str(file_id)
         open(positions_name, "w").write('\n'.join('%lf,%lf,%lf' % (x[i],y[i],z[i]) for i in range(n_points)))
 
+    # Runs the executable "potential.out" that will get the potential energy for each particle
         os.system('./potential.out '+positions_name+' '+potential_name)
 
+    # Finds the particle with the lowest potential and puts it as the new origin
         potential = np.loadtxt(open(potential_name, 'r'))
         maximum = np.argmax(potential)
-
         x_center,y_center,z_center = x[maximum],y[maximum],z[maximum]
-        
-        os.system('rm '+potential_name+' '+positions_name)
-    
+
+    # Gets the radial distance from the new origin to each particle and sorts it
         r_values = np.sort(np.sqrt((x-x_center)**2 + (y-y_center)**2 + (z-z_center)**2), kind='quicksort')
-        
+    
+    # Removes the files used by "potential.out"
+        os.system('rm '+potential_name+' '+positions_name)
+
+    # Gets the mass for each radius and removes the particle in the origin
         mass = mass_element*np.arange(1,n_points,1)
         radius = np.delete(r_values,0)
 
+    # Gets the average density in function of the radius
         avg_density = mass/((4.0/3.0)*np.pi*(radius**3))
         rho_back = mass_element*(2048.0/1000.0)**3
 
-        crit = 740.0
+    # Gets the virial radius
+        if config[8]=='False':
+            crit = 740.0
+            vir_index = np.argmin(np.abs(avg_density-crit*rho_back))
 
-        vir_index = np.argmin(np.abs(avg_density-crit*rho_back))
-
-        if np.argmin(np.abs(avg_density-crit*rho_back)) > 1:
-            r_vir = radius[vir_index]
+            if np.argmin(np.abs(avg_density-crit*rho_back)) > 1:
+                r_vir = radius[vir_index]
+            else:
+                r_vir = radius[-1]
+                vir_index = len(avg_density)-1
         else:
             r_vir = radius[-1]
             vir_index = len(avg_density)-1
-
+            
+    # Removes the particles that have a greater radius than the virial radius
         mass = np.resize(mass,vir_index+1)
         radius = np.resize(radius,vir_index+1)
-	
-	velocity = np.sqrt(G*mass/radius)
-	v_max = np.max(velocity)
-	v_vir = velocity[-1]
-	def f(c):
-		return (v_max/v_vir) - np.sqrt(0.216*c/(np.log(1+c)-c/(1+c))) 
-	
-	c = fsolve(f,10.0)
+
+    # Normalizes the mass and radius
+        mass = mass/mass[-1]
+        radius = radius/radius[-1]
+
+    # Calculates the velocity and finds its maximum
+        velocity = np.sqrt(mass/r)
+        v_max = np.max(velocity)
+    # Defines the relation between the concentration and the greatest velocity
+        f(c):
+            return v_max - np.sqrt(0.216*c/(np.log(1+c)-c/(1+c)))
+
+    # Finds the concentration using f solve
+        c = fsolve(f,10.0)
+
+    # Generates plots
+        if (plt == 1):
+
+            os.system('mkdir '+results_folder)
+            os.chdir(results_folder)
+
+            pylab.plot(radius/radius[-1],velocity/velocity[-1],'.k')
+            pylab.plot(radius/radius[-1],np.sqrt(nfw.mass_norm(radius/radius[-1],c)/(radius/radius[-1])),'-r')
+            pylab.xlabel('$r_{norm}$')
+            pylab.ylabel('$v_{norm}$')
+            pylab.savefig('velocity_'+str(filename)+'.pdf',dpi=200)
+            pylab.close()
+
+            os.chdir('../../')
+            
+    # Writes the results for the halo
         export = open(filename_export, 'a')
         line = [[file_id,x_center,y_center,z_center,c,f(c),r_vir,len(mass),n_points]]
         np.savetxt(export,line,fmt=['%d','%lf','%lf','%lf','%lf','%lf','%lf','%d','%d'],delimiter=',')
