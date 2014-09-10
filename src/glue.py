e5f99701cbe22a30db5b1607ddcaca18ff870182
Christian POveda <christianpoved4@gmail.com>
2014-09-10 08:41:33 -0500

    updated glue tool

--- src/glue.py
+++ src/glue.py
@@ -5,8 +5,6 @@ total_list = os.listdir(directory)
 filenames = sorted([directory+'/'+element for element in total_list if 'result' in element])
 
 export = open(directory+'/results.csv', 'w')
-head = 'Id,x_center,y_center,z_center,c,c_max,c_min,r_vir,n_vir,n_tot'
-
 for fname in filenames:
     with open(fname) as infile:
         for line in infile:
@@ -17,4 +15,9 @@ export.close()
 data = np.loadtxt(directory+'/results.csv',delimiter=',')
 data = data[data[:,0].argsort()]
 
-np.savetxt(directory+'/results.csv',data,fmt='%d,%f,%f,%f,%f,%f,%f,%f,%d,%d',header=head)
+if len(data[0,:]) == 10:
+    head = 'Id,x_center,y_center,z_center,c,c_max,c_min,r_vir,n_vir,n_tot'
+    np.savetxt(directory+'/results.csv',data,fmt='%d,%f,%f,%f,%f,%f,%f,%f,%d,%d',header=head)
+else:
+    head = 'Id,x_center,y_center,z_center,c,f(c),r_vir,n_vir,n_tot'
+    np.savetxt(directory+'/results.csv',data,fmt='%d,%f,%f,%f,%f,%f,%f,%d,%d',header=head)
