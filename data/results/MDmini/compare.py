663676e8ab2c97a6a07dca1e975b1ba43faee16e
Christian POveda <christianpoved4@gmail.com>
2014-09-11 09:42:13 -0500

    new results

--- data/results/MDmini/compare.py
+++ data/results/MDmini/compare.py
@@ -25,78 +25,67 @@ mcmc = np.loadtxt(sys.argv[1],delimiter=',')
 vel = np.loadtxt(sys.argv[2],delimiter=',')
 dens = np.loadtxt(sys.argv[3],delimiter=',')
 
-id_mcmc = mcmc[:,0]
-id_dens = dens[:,0]
+index = [int(x) for x in np.argwhere(dens[:,9] > 100)]
 
-c_mcmc = mcmc[:,4]
-c_vel = vel[:,4]
-c_dens = dens[:,4]
+id_mcmc = mcmc[:,0][index]
+id_dens = dens[:,0][index]
 
-m_mcmc = mcmc[:,8]
-m_vel = vel[:,7]
-m_dens = dens[:,8]
+c_mcmc = mcmc[:,4][index]
+c_vel = vel[:,4][index]
+c_dens = dens[:,4][index]
 
-c_mcmc_dens = np.empty(0)
-c_vel_dens = np.empty(0)
+m_mcmc = mcmc[:,8][index]
+m_vel = vel[:,7][index]
+m_dens = dens[:,8][index]
 
-'''
-pylab.scatter(m_mcmc,c_mcmc,c='b')
-pylab.scatter(m_vel,c_vel,c='y')
-pylab.scatter(m_dens,c_dens,c='r')
-pylab.xscale('log')
-pylab.yscale('log')
-pylab.show()
-'''
 
 bins_mcmc,median_mcmc,quar1_mcmc,quar2_mcmc = get_stats(np.log10(m_mcmc),c_mcmc)
 bins_vel,median_vel,quar1_vel,quar2_vel = get_stats(np.log10(m_vel),c_vel)
 bins_dens,median_dens,quar1_dens,quar2_dens = get_stats(np.log10(m_dens),c_dens)
 
-pylab.plot(10**(bins_mcmc),median_mcmc,'-g',lw=2,label='Mass')
-pylab.fill_between(10**(bins_mcmc),quar1_mcmc,quar2_mcmc,color='g',alpha=0.4)
+pylab.plot(10**(bins_dens),median_dens,'-g',lw=2,label='Density')
+pylab.fill_between(10**(bins_dens),quar1_dens,quar2_dens,color='g',alpha=0.4)
+
+pylab.plot(10**(bins_mcmc),median_mcmc,'-r',lw=2,label='Mass')
+pylab.fill_between(10**(bins_mcmc),quar1_mcmc,quar2_mcmc,color='r',alpha=0.4)
+
 pylab.plot(10**(bins_vel),median_vel,'-b',lw=2,label='Velocity')
 pylab.fill_between(10**(bins_vel),quar1_vel,quar2_vel,color='b',alpha=0.4)
-pylab.plot(10**(bins_dens),median_dens,'-r',lw=2,label='Density')
-pylab.fill_between(10**(bins_dens),quar1_dens,quar2_dens,color='r',alpha=0.4)
+
+
 pylab.legend(loc=1, borderaxespad=0.5)
 pylab.xscale('log')
-pylab.xlim([1E2,1E5])
+pylab.xlim([1E3,1E5])
 pylab.xlabel('$Number\ of\ Particles$')
 pylab.ylabel('$Concentration$')
 pylab.savefig('concentration.pdf',dpi=300)
 pylab.close()
 
-
-for id in id_dens:
-    index = np.where(id_mcmc == id)
-    c_mcmc_dens = np.append(c_mcmc_dens,c_mcmc[index])
-    c_vel_dens = np.append(c_vel_dens,c_vel[index])
-
 pylab.scatter(c_vel,c_mcmc,c='k',alpha=0.1)
 pylab.xscale('log')
 pylab.yscale('log')
-pylab.xlim(xmin=np.amin(c_vel))
-pylab.ylim(ymin=np.amin(c_mcmc))
+pylab.xlim(xmin=np.amin(c_vel),xmax=np.amax(c_vel))
+pylab.ylim(ymin=np.amin(c_vel),ymax=np.amax(c_vel))
 pylab.xlabel("$Velocity\ Method$")
 pylab.ylabel("$Mass\ Method$")
 pylab.savefig('velocity-mass.pdf',dpi=200)
 pylab.close()
 
-pylab.scatter(c_dens,c_mcmc_dens,c='k',alpha=0.1)
+pylab.scatter(c_mcmc,c_dens,c='k',alpha=0.1)
 pylab.xscale('log')
 pylab.yscale('log')
-pylab.xlim(xmin=np.amin(c_dens))
-pylab.ylim(ymin=np.amin(c_mcmc_dens))
-pylab.xlabel("$Density\ Method$")
-pylab.ylabel("$Mass\ Method$")
-pylab.savefig('density-mass.pdf',dpi=200)
+pylab.xlim(xmin=np.amin(c_dens),xmax=np.amax(c_mcmc))
+pylab.ylim(ymin=np.amin(c_dens),ymax=np.amax(c_mcmc))
+pylab.xlabel("$Mass\ Method$")
+pylab.ylabel("$Density\ Method$")
+pylab.savefig('mass-density.pdf',dpi=200)
 pylab.close()
 
-pylab.scatter(c_dens,c_vel_dens,c='k',alpha=0.1)
+pylab.scatter(c_dens,c_vel,c='k',alpha=0.1)
 pylab.xscale('log')
 pylab.yscale('log')
-pylab.xlim(xmin=np.amin(c_dens))
-pylab.ylim(ymin=np.amin(c_vel_dens))
+pylab.xlim(xmin=np.amin(c_dens),xmax=np.amax(c_vel))
+pylab.ylim(ymin=np.amin(c_dens),ymax=np.amax(c_vel))
 pylab.xlabel("$Density\ Method$")
 pylab.ylabel("$Velocity\ Method$")
 pylab.savefig('density-velocity.pdf',dpi=200)
