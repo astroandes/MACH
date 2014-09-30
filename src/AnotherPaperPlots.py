a41fd9b1e5baa8c0a6051e15cb93c29ac4290f3a
Christian Poveda <christianpoved4@gmail.com>
2014-09-30 14:49:29 -0500

    added new plots

--- src/AnotherPaperPlots.py
+++ src/AnotherPaperPlots.py
@@ -8,8 +8,21 @@ for c in C:
 pylab.legend(loc=0, scatterpoints=1, prop={'size':20})
 filename = 'nfw_normalized'
 matplotlib.rcParams.update({'font.size': 18})
-pylab.xlabel('$r_{norm}$',fontsize=20)
-pylab.ylabel('$m_{norm}$',fontsize=20)
+pylab.xlabel('$x$',fontsize=20)
+pylab.ylabel('$m(<x)$',fontsize=20)
+pylab.savefig(filename+'.eps',transparent=True)
+pylab.savefig(filename+'.pdf',transparent=True)
+pylab.savefig(filename+'.png',transparent=True)
+pylab.close()
+
+
+for c in C:
+    pylab.plot(r,np.sqrt(nfw.mass_norm(r,c)/r),color='black',lw= np.log(c+1)/2,label="$c="+str(c)+"$")
+pylab.legend(loc=0, scatterpoints=1, prop={'size':20})
+filename = 'vel_normalized'
+matplotlib.rcParams.update({'font.size': 18})
+pylab.xlabel('$x$',fontsize=20)
+pylab.ylabel('$v(<x)$',fontsize=20)
 pylab.savefig(filename+'.eps',transparent=True)
 pylab.savefig(filename+'.pdf',transparent=True)
 pylab.savefig(filename+'.png',transparent=True)
